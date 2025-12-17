ba9fcc054c3d6132e123d339a9e9cfeb83db1fef
Swapnil-Jagtap <jagtapswapnil2303@gmail.com>
2025-12-17 13:56:54 +0530

    Controller change

--- backend/purandhar-website-backend/src/main/java/com/purandhar/website/controller/NewsletterController.java
+++ backend/purandhar-website-backend/src/main/java/com/purandhar/website/controller/NewsletterController.java
@@ -2,19 +2,26 @@ package com.purandhar.website.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.purandhar.website.model.ApiResponse;
 import com.purandhar.website.model.NewsletterSubscriber;
 import com.purandhar.website.service.NewsletterService;
 
+@CrossOrigin(
+	    origins = "https://purandhartechnologiesprod.vercel.app",
+	    allowedHeaders = "*",
+	    methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS}
+	)
 
 @RestController
 @RequestMapping("/api/newsletter")
-//@CrossOrigin(origins = "*")
+
 public class NewsletterController {
 
 	@Autowired
