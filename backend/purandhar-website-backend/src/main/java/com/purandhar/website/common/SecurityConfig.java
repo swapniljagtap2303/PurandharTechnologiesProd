2f5522cd9859331aa41cdcd7e39ad7c798374c50
Swapnil-Jagtap <jagtapswapnil2303@gmail.com>
2025-12-17 21:09:26 +0530

    9.09

---
+++ backend/purandhar-website-backend/src/main/java/com/purandhar/website/common/SecurityConfig.java
@@ -0,0 +1,27 @@
+//package com.purandhar.website.common;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        http
+//            .cors(Customizer.withDefaults())
+//            .csrf(csrf -> csrf.disable())
+//            .authorizeHttpRequests(auth -> auth
+//                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//                .requestMatchers("/api/**").permitAll()
+//                .anyRequest().authenticated()
+//            );
+//
+//        return http.build();
+//    }
+//}
