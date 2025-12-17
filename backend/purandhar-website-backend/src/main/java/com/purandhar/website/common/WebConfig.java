9a9cc11a397dff5b55bae273832d946a812ffa2b
Swapnil-Jagtap <jagtapswapnil2303@gmail.com>
2025-12-17 10:04:10 +0530

    Change in webconfig

--- backend/purandhar-website-backend/src/main/java/com/purandhar/website/common/WebConfig.java
+++ backend/purandhar-website-backend/src/main/java/com/purandhar/website/common/WebConfig.java
@@ -13,11 +13,14 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200") // Angular app
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                            "http://localhost:4200",
+                            "https://purandhartechnologiesprod.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false);
             }
         };
     }
